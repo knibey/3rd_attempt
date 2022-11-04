7afc035de66d25e98b876acf20f38f53f5e1ec02
knibey <clevertea22@gmail.com>
2022-11-04 19:20:27 +0200

    changes

--- visualstudiocode/visualstudiocode.cpp
+++ visualstudiocode/visualstudiocode.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     
-/*    int a = getValueFromUser();
+    int a = getValueFromUser();
     int b = getValueFromUser();
 
     std::cout << a << " + "  << b << " = " << a + b << std::endl;
@@ -14,14 +14,14 @@ int main()
 
     std::cout << "the sum is - " << sum(&a, &b) << std::endl;
 
-    std::cout << "the double number of first number is - " << doubleNumber(a) << std::endl;*/
+    std::cout << "the double number of first number is - " << doubleNumber(a) << std::endl;
 
-    std::cout << "Enter your first number: " << std::endl;
+    /*std::cout << "Enter your first number: " << std::endl;
     int uservalue1 = readNumber();
     std::cout << "Enter your second number: " << std::endl;
     int uservalue2 = readNumber();
     
-    std::cout << "Here is your result: " << writeAnswer(uservalue1, uservalue2) << std::endl;
+    std::cout << "Here is your result: " << writeAnswer(uservalue1, uservalue2) << std::endl;*/
 
     return 0;
 }
