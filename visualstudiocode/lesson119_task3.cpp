e56ef3df674531672b8730191f91ed6755a46ae4
knibey <clevertea22@gmail.com>
2023-03-03 13:06:51 +0200

    lesson119 task 3 rerun

--- visualstudiocode/lesson119_task3.cpp
+++ visualstudiocode/lesson119_task3.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <array>
 #include <iostream>
 #include <string_view>
@@ -9,63 +8,110 @@
 #include <cstring>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 int getRandomNumbers(int min, int max) {
     static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
-std::vector<int> getVector(int size, int randomnumber, int startnumber) {
-    std::vector<int> numbers{ 0 };
-    numbers.resize(size);
+std::vector<int> getVector(int startnumber, int count, int random) {
+    std::vector<int> numbers(static_cast<std::vector<int>::size_type>(count));
 
-    for (int i = 0; i < size; i++) {
-        numbers[i] = pow(startnumber, 2);
-        startnumber++;
-    }
+    int i{ startnumber };
 
-    for (int i = 0; i < size; i++) {
-        numbers[i] *= randomnumber;
+    for (auto& number : numbers) {
+        number = ((i*i) * random);
+        i++;
     }
 
     return numbers;
-};
+}
+
+std::vector<int> generateVector(int random) {
+    std::cout << "Start where? ";
+    int startnumber = getIntFromUser();
+
+    std::cout << "How many? ";
+    int count = getIntFromUser();
+
+    return getVector(startnumber, count, random);
+}
+
+int getUserGuess() {
+    int user_guess = getIntFromUser();
+
+    return user_guess;
+}
+
+bool removeOrNot(std::vector<int>& numbers, int user_guess) {
+    auto found(std::find(numbers.begin(), numbers.end(), user_guess));
+
+    if (found == numbers.end()) {
+        return false;
+    }
+    else {
+        numbers.erase(found);
+        return true;
+    }
+}
+
+int closestOne(const std::vector<int>& numbers, int user_guess) {
+    return *std::min_element(numbers.begin(), numbers.end(), [=](int a, int b) {
+        return (std::abs(user_guess - a) < (std::abs(user_guess - b))); });
+}
+
+void printTask(std::vector<int>::size_type count, int random) {
+    std::cout << "I generated " << count << " square numbers. Do you know what each number is after multiplying it by " << random << " ?" << std::endl;
+}
+
+void printWin(std::vector<int>::size_type left) {
+    std::cout << "Nice" << std::endl;
+
+    if (left == 0) {
+        std::cout << "You won!" << std::endl;
+    }
+    else {
+        std::cout << left << " number(s) left." << std::endl;
+    }
+}
+
+void printLose(const std::vector<int>& numbers, int user_guess) {
+    int closest{ closestOne(numbers, user_guess) };
+
+    std::cout << user_guess << " is wrong." << std::endl;
+
+    if (std::abs(closest - user_guess) <= 4) {
+        std::cout << "Try " << closest << " next time." << std::endl;
+    } else {
+        std::cout << "LOH" << std::endl;
+    }
+}
+
+bool playGame(std::vector<int>& numbers) {
+    int user_guess{ getUserGuess() };
+
+    if (removeOrNot(numbers, user_guess)) {
+        printWin(numbers.size());
+
+        return !numbers.empty();
+    } else {
+        printLose(numbers, user_guess);
+
+        return false;
+    }
+}
 
 int main() {
     srand(static_cast<unsigned int>(time(0)));
     rand();
 
-    int randomnumber = getRandomNumbers(2, 4);
-    std::cout << "random number is: " << randomnumber << std::endl;
+    int random = getRandomNumbers(2, 4);
+    std::vector<int> numbers{ generateVector(random) };
 
-    std::cout << "Start where? " << std::endl;
-    int startnumber = getIntFromUser();
+    printTask(numbers.size(), random);
 
-    std::cout << "How many" << std::endl;
-    int count = getIntFromUser();
-
-    std::vector<int> numbers = getVector(count, randomnumber, startnumber);
-
-    for (int i = 0; i < count; i++) {
-        std::cout << numbers[i] << " ";
-    }
-
-    std::cout << "\nI generated " << count << " square numbers. Do you know what each number is after multiplying it by " << randomnumber <<" ?" << std::endl;
-
-    int user_guess{ 0 };
-
-    while (numbers.size() != 0) {
-        user_guess = getIntFromUser();
-
-        auto found{ std::find(numbers.begin(), numbers.end(), user_guess) };
-
-        if (found != std::end(numbers)) {
-            std::cout << "Nice!" << std::endl;
-        }
-
-        numbers.erase(found);
-
-    }
+    while(playGame(numbers));
 
     return 0;
 }
