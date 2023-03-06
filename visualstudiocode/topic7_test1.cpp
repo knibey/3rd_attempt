3e1aaca622f7b422ac97d15bfa5a7c6044cdf28a
knibey <clevertea22@gmail.com>
2023-03-06 09:22:05 +0200

    topic7_test1 and topic7_test3(partially)

---
+++ visualstudiocode/topic7_test1.cpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <iostream>
+#include <string_view>
+#include "library.h"
+#include "constants.h"
+#include <string>
+#include <utility>
+#include <cstring>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+double max(const double first, const double second);
+
+void swap(int &a, int &b);
+
+int& getLargestElement(int *arr, int size);
---
+++ visualstudiocode/topic7_test3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string_view>
+#include "library.h"
+#include "constants.h"
+#include <string>
+#include <utility>
+#include <cstring>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+int binarySearch(int* array, int target, int min, int max) {
+    while (min <= max) {
+        int mid = (max + min) / 2;
+        
+        if (array[mid] > target) {
+            max = mid - 1;
+        }
+        
+        if (array[mid] < target) {
+            min = mid + 1;
+        }
+        
+        if (array[mid] == target) {
+            return mid;
+        }
+    }
+
+    return -1;
+
+}
+
+int binarySearchrec(int* array, int target, int min, int max) {
+}
+
+int main()
+{
+    int array[] = { 4, 7, 9, 13, 15, 19, 22, 24, 28, 33, 37, 41, 43, 47, 50 };
+
+    std::cout << "Enter a number: ";
+    int x = getIntFromUser();
+
+    int index = binarySearch(array, x, 0, 14);
+
+    if (array[index] == x)
+        std::cout << "Good! Your value " << x << " is on position " << index << " in array!\n";
+    else
+        std::cout << "Fail! Your value " << x << " isn't in array!\n";
+    return 0;
+}
